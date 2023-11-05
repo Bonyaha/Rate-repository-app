37430c50d5736d2f66b44b60e7703f447351d0e9
Roman <romchikus1987@gmail.com>
2023-11-05 18:47:05 +0200

    updated font

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native'
+
 const theme = {
 	colors: {
 		textPrimary: '#24292e',
@@ -9,7 +11,11 @@ const theme = {
 		subheading: 16,
 	},
 	fonts: {
-		main: 'System',
+		main: Platform.select({
+			android: 'Roboto',
+			ios: 'Arial',
+			default: 'System', // Use System as a default font
+		}),
 	},
 	fontWeights: {
 		normal: '400',
